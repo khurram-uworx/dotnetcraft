ca8b950f27888b10d33471367f06dffa3209bcea
khurram-uworx <khurram.aziz@uworx.co.uk>
2026-07-10 12:50:16 +0500

    encapsulation

---
+++ src/TestProjects/WorkplaceIQTests/SocialMediaTests.cs
@@ -0,0 +1,25 @@
+﻿using UWorx.WorkplaceIQ;
+
+namespace WorkplaceIQTests
+{
+    public class SocialMediaTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            SocialMediaPost p = SocialMediaPost.GetInstance(
+                platform: "linkedin",
+                text: "I am learning OOP");
+                //.UpdateLabels(["label1", "label2"]); // is optional
+            
+            p.StoreIntoDb();
+
+            Assert.Pass();
+        }
+    }
+}
---
+++ src/UWorx.WorkplaceIQ/SocialMediaPost.cs
@@ -0,0 +1,57 @@
+﻿namespace UWorx.WorkplaceIQ
+{
+    public class SocialMediaPost
+    {
+        private static class Helper
+        {
+            public static void DoSomething()
+            { }
+
+            public static void DoSomething2()
+            { }
+
+            public static void DoSomething3()
+            { }
+        }
+
+        string platform;
+        string text;
+        List<string> labels;
+
+        private SocialMediaPost()
+        {
+
+        }
+
+        private SocialMediaPost(string platform, string text, string[] labels)
+        { }
+
+        public static SocialMediaPost GetInstance(string platform, string text)
+        {
+            return new SocialMediaPost()
+                .UpdatePlatform(platform)
+                .UpdateText(text);
+        }
+
+        public SocialMediaPost UpdatePlatform(string platform)
+        {
+            this.platform = platform;
+            return this;
+        }
+
+        public SocialMediaPost UpdateText(string text)
+        {
+            this.text = text;
+            return this;
+        }
+
+        public SocialMediaPost UpdateLabels(string[] labels)
+        {
+            this.labels.AddRange(labels);
+            return this;
+        }
+
+        public void StoreIntoDb()
+        { }
+    }
+}
